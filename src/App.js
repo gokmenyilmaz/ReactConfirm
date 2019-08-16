863b7c8bc49f42df65bc00189e6f5362804fb752
gokmen.yilmaz <gokmen1977@gmail.com>
2019-08-17 00:07:57 +0300

    hh

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import confirmService from './confirmService'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        'Eat hamburger',
+        'Drink coke',
+        'Go to bathroom'
+      ]
+    };
+    this.removeItem = this.removeItem.bind(this);
+  }
+
+  async removeItem({ target: { value } }) {
+    const result = await confirmService.show({
+      message: 'Are you sure of delete this item?'
+    });
+    if (result) {
+      const items = this.state.items.filter((item, index) => index !== parseInt(value));
+      this.setState({ items });
+    }
+
+    console.log(value);
+  }
+
+  render() {
+    const { items } = this.state;
+    return (
+      <div className="container-fluid">
+        <h1 className="is-size-2">React Confirm as a Service</h1>
+        <p>try to delete one</p>
+        <h2>TODO:</h2>
+        <div className="columns is-mobile is-multiline">
+          {
+            items.map((item, index) => (
+            <div className="column is-6" key={item}>
+              <div className="card">
+                <div className="card-content">
+                  <p className="title">{item}</p>
+                </div>
+                <footer className="card-footer">              
+                  <p className="card-footer-item">
+                    <button 
+                      className="button is-danger"
+                      value={index}
+                      onClick={this.removeItem}
+                    >
+                      Delete
+                    </button>
+                  </p>
+                </footer>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
